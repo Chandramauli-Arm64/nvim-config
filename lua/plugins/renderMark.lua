9769f75747552ba335b05b3852a196b6a28e961c
Chandramauli-Arm64 <chandramauli8986@gmail.com>
2025-07-01 13:59:48 +0530

    Added Render Markdown plugin

--- lua/plugins/renderMark.lua
+++ lua/plugins/renderMark.lua
@@ -1,7 +1,16 @@
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
+  'MeanderingProgrammer/render-markdown.nvim',
+  ft = { "markdown" },
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
+  },
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+    render_on_start = false, -- Prevent lag on load
+  },
+  keys = {
+    { "<leader>mr", function() require("render-markdown").toggle() end, desc = "Toggle Render Markdown" },
+  },
 }
