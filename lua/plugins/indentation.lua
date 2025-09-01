474ad871a9797272207d528d1ff5d06eeb411722
Chandramauli-Arm64 <189079276+Chandramauli-Arm64@users.noreply.github.com>
2025-09-01 16:16:03 +0530

    Feat(indentation): UPDATED
    ---
    
    Updated indentation.lua

--- lua/plugins/indentation.lua
+++ lua/plugins/indentation.lua
@@ -1,15 +1,24 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
+  ---@module "ibl"
+  ---@diagnostic disable-next-line: undefined-doc-name
+  ---@type ibl.config
   opts = {
+    -- Character for indentation
     indent = {
       char = "▏",
     },
+
+    -- Current scope highlight
     scope = {
-      show_start = false,
-      show_end = false,
-      show_exact_scope = false,
+      enabled = true,
+      show_start = true, -- glow at the start of scope
+      show_end = false, -- don't highlight end
+      show_exact_scope = false, -- only current scope
     },
+
+    -- Exclude certain filetypes
     exclude = {
       filetypes = {
         "help",
